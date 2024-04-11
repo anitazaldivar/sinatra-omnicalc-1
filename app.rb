01aa4d457432890efa6d0cb0057631460455d628
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-04-11 21:02:59 +0000

    first page ready

--- app.rb
+++ app.rb
@@ -7,3 +7,13 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get '/square/new' do
+  erb(:new_square_calc)
+end
+
+get '/square/results' do
+  @the_number = params.fetch("number")
+  @the_result = params.fetch("number").to_f**2
+  erb(:square_results)
+end
